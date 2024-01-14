00aa613a1713995e849c4bf7e36878849453feb0
ahmed-fawzy-hub <af348565@gmail.com>
2024-01-14 16:43:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mysqlitefull/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mysqlitefull
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btn : Button
+    lateinit var btn2 : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.databaseBtn)
+        btn2 = findViewById(R.id.insertDB)
+        btn.setOnClickListener {
+            val intent = Intent(this,DatabaseActivity2::class.java)
+            startActivity(intent)
+
+        }
+        btn2.setOnClickListener {
+            val intent = Intent(this,InsertActivity2::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
